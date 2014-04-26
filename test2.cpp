864216839ebfa7f43cd164bb0e52238707b51ad1
KevinHu2014 <rogerhu61rogerhu61@yahoo.com.tw>
2014-04-26 18:17:00 +0800

    駱

--- test2.cpp
+++ test2.cpp
@@ -6,7 +6,7 @@ int main()
     char x[30];
     int i;
 
-    scanf("%s", x);//%c-->¦r¤¸/%s-->¦r¦ê
+    scanf("%s", x);//%c-->character/%s-->string/your input is a string not a single character
 
     for(i = 0;x[i] != '\0';i++){
         printf("%c",x[i]);
